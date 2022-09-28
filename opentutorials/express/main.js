0cf8e07d1621eb82b1cfcc0f963cf2c1c1669812
Phabala <kihong2424@gmail.com>
2022-09-28 21:51:39 +0900

    Express - MiddleWare

--- opentutorials/express/main.js
+++ opentutorials/express/main.js
@@ -1,25 +1,159 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs');
+const path = require('path');
+const template = require('./lib/template.js');
+const sanitizeHtml = require('sanitize-html');
+const qs = require('querystring');
+const bodyParser = require('body-parser');
+const compression = require('compression')
+// Express에서는 모든 것이 Middleware
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(compression());
+app.get('*', function(request, response, next){ // next에는 그 다음에 호출되야할 middleware가 담겨있다.
+  fs.readdir('./data', function(error, filelist) {
+    request.list = filelist;
+    next();
+  });
+});
+
 
 // get 메소드: route, routing (사용자가 패스마다 들어올 때 적당한 응답을 하는 것.)
+/*
 app.get('/', (req, res) => {
   res.send('/') // 화살표 함수의 유일한 문장이 return일 때, return과 중괄호를 생략할 수 있다.
 });
+*/
 
-app.get('/page', (req, res) => {
-  res.send('<h1>page</h1><hr>') // 여기에 template.js 를 저장한 변수를 넣어도 되겠다.
+app.get('/', function(request, response) {
+    var title = 'Welcome';
+      var description = 'Hello, Node.js';
+      var list = template.list(request.list);
+      var html = template.HTML(title, list,
+        `<h2>${title}</h2>${description}`,
+        `<a href="/create">create</a>`
+      );
+      response.send(html); // response.writeHead(200); response.end(html); writeHead랑 end를 send 하나로 가능.
 });
 
-/* 
-app.get('/', function(req, res) {
-  return res.send('Hello World!')
-}); // 위와 코드와 이것은 같은 것. 화살표 함수는 return 을 안 해줘도 되나?
-*/
+app.get('/page/:pageId', (request, response) => {
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(request.list);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update/${sanitizedTitle}">update</a>
+          <form action="/delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    });
+});
+
+app.get('/create', (request, response) => {
+    var title = 'WEB - create';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list, `
+      <form action="/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+          <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+          <input type="submit">
+        </p>
+      </form>
+    `, '');
+    response.send(html);
+});
+
+app.post('/create_process', (request, response) => { // get과 poset방식이 다르기 때문에 create_process가 아니라 create로 하면, 어떤 method로 받냐에 따라 갈린다.
+  console.log(request.list);
+  var post = request.body; // qs.parse(body) 를 request.body 가 대체.
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.writeHead(302, {Location: `/?id=${title}`});
+      response.end();
+    })
+  });
+
+app.get('/update/:pageId', (request, response) => {
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageId;
+      var list = template.list(request.list);
+      var html = template.HTML(title, list,
+        `
+        <form action="/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `,
+        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+      );
+      response.send(html);
+    });
+})
+
+app.post('/update_process', (request, response) => {
+    var post = request.body; // 이게 왜 되지
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function(error){
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        response.redirect(`/page/${title}`);
+      })
+    });
+});
+
+app.post('/delete_process', (request, response) => {
+    var post = request.body;
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function(error){
+      // response.writeHead(302, {Location: `/`}); --> redirect로 대체 가능
+      response.redirect('/');
+    })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+  /* body-parser를 이용하여 잘라낸 부분.
+  var body = '';
+  request.on('data', function(data){
+    body = body + data;
+  });
+  request.on('end', function(){
+    var post = qs.parse(body);
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.writeHead(302, {Location: `/?id=${title}`});
+      response.end();
+    })
+  });
+});
+*/
 
 
 /*
